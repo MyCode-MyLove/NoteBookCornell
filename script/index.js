602aa47fdb7ef6f6006d88f84d6d1382bca3df9b
To.Ly.My <148930434+MyCode-MyLove@users.noreply.github.com>
2025-02-13 22:05:04 +0700

    first add

---
+++ script/index.js
@@ -0,0 +1,21 @@
+let url, new_url;
+url = window.location.href;
+new_url = url.split('?')[0];
+window.history.pushState({}, document.title, new_url);
+
+function toggleMenu() {
+    var menu = document.getElementById("mobileMenu");
+    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}
+document.addEventListener("DOMContentLoaded", function () {
+    var links = document.querySelectorAll(".left a");
+    var currentPath = window.location.pathname;
+
+    links.forEach(function (link) {
+        if (link.href.includes(currentPath)) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
+});
